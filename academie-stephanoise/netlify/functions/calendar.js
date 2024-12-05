8d475a8b65e8caf3ffc5f32b0453cd222b177efa
Thomas Farre <th.farre@gmail.com>
2024-12-05 09:37:40 +0100

    debug

--- academie-stephanoise/netlify/functions/calendar.js
+++ academie-stephanoise/netlify/functions/calendar.js
@@ -11,21 +11,30 @@ const HEADERS = {
 
 exports.handler = async function (event, context) {
   const finalURL = `${BASEURL}&key=${CALENDAR_API}`;
+  console.log("Final URL:", finalURL);
+  console.log("Event:", event);
   try {
     if (event.httpMethod === "GET") {
       return fetch(finalURL)
-        .then((response) => response.json())
-        .then((data) => ({
-          statusCode: 200,
-          body: JSON.stringify(data.items, null, 2),
-          HEADERS,
-        }));
+        .then((response) => {
+          console.log("Response status:", response.status);
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Data received:", data);
+          return {
+            statusCode: 200,
+            body: JSON.stringify(data.items, null, 2),
+            HEADERS,
+          };
+        });
     }
+    console.log("Invalid HTTP method:", event.httpMethod);
     return {
       statusCode: 401,
     };
   } catch (e) {
-    console.error(e);
+    console.error("Error occurred:", e);
     return {
       statusCode: 500,
       body: e.toString(),
