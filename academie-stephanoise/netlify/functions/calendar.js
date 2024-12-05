e176b624e5390aaa932f9785f1cb5c967f5b8114
Thomas Farre <th.farre@gmail.com>
2024-12-05 11:28:59 +0100

    debug

--- academie-stephanoise/netlify/functions/calendar.js
+++ academie-stephanoise/netlify/functions/calendar.js
@@ -1,34 +1,42 @@
-import fetch from "node-fetch";
-
-const { CALENDAR_API, CALENDAR_ID } = process.env;
-const BASEPARAMS = `orderBy=startTime&singleEvents=true&timeMin=${new Date().toISOString()}`;
-const BASEURL = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?${BASEPARAMS}`;
-
-const HEADERS = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Methods": "GET",
-};
-
 exports.handler = async function (event, context) {
-  const finalURL = `${BASEURL}&key=${CALENDAR_API}`;
   try {
-    if (event.httpMethod === "GET") {
-      return fetch(finalURL)
-        .then((response) => response.json())
-        .then((data) => ({
-          statusCode: 200,
-          body: JSON.stringify(data.items, null, 2),
-          HEADERS,
-        }));
+    const BASEPARAMS = `orderBy=startTime&singleEvents=true&timeMin=${new Date().toISOString()}`;
+    const BASEURL = `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_ID}/events?${BASEPARAMS}`;
+    const finalURL = `${BASEURL}&key=${process.env.CALENDAR_API}`;
+
+    console.log("Final URL:", finalURL);
+    console.log("Environment Variables:", {
+      CALENDAR_API: process.env.CALENDAR_API,
+      CALENDAR_ID: process.env.CALENDAR_ID,
+    });
+
+    const response = await fetch(finalURL);
+    console.log("API Response Status:", response.status);
+
+    const data = await response.json();
+    console.log("API Response Body:", JSON.stringify(data, null, 2));
+
+    if (!Array.isArray(data.items)) {
+      console.error("Unexpected Data Format:", data);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Invalid data format" }),
+      };
     }
+
     return {
-      statusCode: 401,
+      statusCode: 200,
+      body: JSON.stringify(data.items),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
     };
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    console.error("Error fetching events:", error);
     return {
       statusCode: 500,
-      body: e.toString(),
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
